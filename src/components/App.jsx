1c882a3bbbf4d80f7cde5806f0e41cab0c61d9f8
Koro0 <ledane92@gmail.com>
2022-10-17 10:49:12 +0200

    add path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import '../styles/App.css';
-import Header from './Header';
-import Home from './Home/Home'
+import Home from './Home/Home';
+import Connexion from './LogPage/LogPage';
+import Header from './Header/index';
+//import NewPost from './Home/NewPost/NewPost';
+
 
 function App() {
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path='/' element={<Connexion />} />
         <Route path='/Home' element={<Home />} />
       </Routes>
     </BrowserRouter>
-
   );
+
 }
 
 export default App;
