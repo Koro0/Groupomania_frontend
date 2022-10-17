fe01d2896a2435a59d51aad6e090ac65d8bf04dd
Koro0 <ledane92@gmail.com>
2022-10-17 11:01:07 +0200

    Clean Code

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,3 @@
-import 'primeicons/primeicons.css';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';
-import 'primereact/resources/primereact.css';
-import 'primeflex/primeflex.css';
 import '../../styles/home.css';
 
 import React, { useState, useEffect } from 'react';
